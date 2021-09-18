7d44cc59fb73094f5d8d36e5e8e41d137655a1da
Boris Sobolev <rafinad@outlook.com>
2021-09-18 16:06:54 +0300

    Initial Commit

---
+++ PlannerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlannerApp
+//
+//  Created by Boris Sobolev on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
