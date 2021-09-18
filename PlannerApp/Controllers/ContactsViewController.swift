bf1c3c9b871d632f882027183b36c79670c72f8a
Boris Sobolev <rafinad@outlook.com>
2021-09-18 16:45:33 +0300

    add tabor and navcontrollers

---
+++ PlannerApp/Controllers/ContactsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsViewController.swift
+//  PlannerApp
+//
+//  Created by Boris Sobolev on 18.09.2021.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Contacts"
+    }
+}
